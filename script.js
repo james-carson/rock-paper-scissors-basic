4f0ac2a2059a331f7c2f23685f2d103639990986
carsoninho <james.87.carson@gmail.com>
2024-04-09 12:01:16 +0200

    Complete process of playGame without scoring
    Now need to work out how to log score.

--- script.js
+++ script.js
@@ -33,78 +33,128 @@ function getPlayerChoice() {
 
 // 4) Play a game. Nested IF functions x 3. Compare two selections and return a declaration.
 
-let playerScore = 0
-let computerScore = 0
-
-let computerChoice = getComputerChoice()
-console.log(computerChoice)
-let playerChoice = getPlayerChoice()
-console.log(playerChoice);
-
 function playRound(playerChoice, computerChoice) {
-
-    let result = null
 
     if (playerChoice === "Rock") {
         if (computerChoice === "Rock") {
-            alert("It's a draw!")
-            return result = "draw"
+            alert("It's a draw!");
+            let result = "draw";
+            return result;
         }
         else if (computerChoice === "Paper") {
-            alert("You lose! Paper beats Rock!")
-            return result = "lose"
+            alert("You lose! Paper beats Rock!");
+            let result = "lose";
+            return result;
         }
         else if (computerChoice === "Scissors") {
-            alert("You win! Rock beats Paper!")
-            return result = "win"
+            alert("You win! Rock beats Paper!");
+            let result = "win";
+            return result;
         }
         else{
-            alert("Something fucked up somewhere");
+            alert("Something fucked up somewhere. You probably entered something incorrectly.");
         }
 
     }
     else if (playerChoice === "Paper") {
         if (computerChoice === "Rock") {
-            alert("You win! Paper beats Rock!")
-            return result = "win"
+            alert("You win! Paper beats Rock!");
+            let result = "win";
+            return result;
         }
         else if (computerChoice === "Paper") {
-            alert("It's a draw!")
-            return result = "draw"
+            alert("It's a draw!");
+            let result = "draw";
+            return result;
         }
         else if (computerChoice === "Scissors") {
-            alert("You lose! Scissors beats Paper!")
-            return result = "lose"
+            alert("You lose! Scissors beats Paper!");
+            let result = "lose";
+            return result;
         }
         else{
-            alert("Something fucked up somewhere");
+            alert("Something fucked up somewhere. You probably entered something incorrectly.");
         }
     }
     else if (playerChoice === "Scissors") {
         if (computerChoice === "Rock") {
-            alert("You lose! Rock beats Scissors!")
-            return result = "lose"
+            alert("You lose! Rock beats Scissors!");
+            let result = "lose";
+            return result;
         }
         else if (computerChoice === "Paper") {
-            alert("You win! Scissors beats Paper!")
-            return result = "win"
+            alert("You win! Scissors beats Paper!");
+            let result = "win";
+            return result;
         }
         else if (computerChoice === "Scissors") {
-            alert("It's a draw!")
-            return result = "draw"
+            alert("It's a draw!");
+            let result = "draw";
+            return result;
         }
         else{
-            alert("Something fucked up somewhere");
+            alert("Something fucked up somewhere. You probably entered something incorrectly.");
         }
     }
     else {
-        alert("Something fucked up somewhere");
+        alert("Something fucked up somewhere. You probably entered something incorrectly.");
     }
 
-    console.log(result)
-    
 }
 
-playRound(playerChoice, computerChoice)
+// Need to work out logging of result now.
 
 // 5) Create playGame function. Ensure that there are 5 rounds and a winner is reported at the end.
+
+function playGame() {
+    // Set score
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // Select R/P/S 1
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+
+    // Game 1
+    playRound(playerChoice, computerChoice);
+
+    // Update Score 1
+
+    // Select R/P/S 2
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    // Game 2
+    playRound(playerChoice, computerChoice);
+
+    // Update Score 2
+
+    // Select R/P/S 3
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    // Game 3
+    playRound(playerChoice, computerChoice);
+
+    // Update Score 3
+
+    // Select R/P/S 4
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    // Game 4
+    playRound(playerChoice, computerChoice)
+
+    // Update Score 4
+
+    // Select R/P/S 5
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    // Game 5
+    playRound(playerChoice, computerChoice);
+
+    // Update Score 5
+}
+
+playGame()
